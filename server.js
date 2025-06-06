b6f44d3f819459e5a446c1c998e9e0b5babff177
Ahmed Ali <ahmedaliqurexhi7867@gmail.com>
2025-06-07 00:49:27 +0500

    Portfolio

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine","ejs");
-app.set("views","./views");
+app.use(express.static('public'));
 
-app.get("/",(req,res)=>{
-    res.render("portfolio");
-})
-
-app.listen(4000, () => {
-  console.log('✅ Server start at http://localhost:4000');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/portfolio.ejs');
 });
+
+module.exports = app;
